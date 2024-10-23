465312cb0b099b0c45368808aff201eb372c6cf7
Aatisha Cyrill <cyrill@wisc.edu>
2024-10-23 00:23:48 -0500

    added banner

---
+++ src/app/components/banner-top/banner-top.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banner-top',
+  standalone: true,
+  imports: [],
+  templateUrl: './banner-top.component.html',
+  styleUrl: './banner-top.component.scss'
+})
+export class BannerTopComponent {
+
+}
